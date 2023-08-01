3665f833ca131814df38f798ec70842e8ee258ac
Ugur Emirmustafa <uguremirmustafa@gmail.com>
2023-08-01 19:21:24 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+function test(s: string): number {
+  let substring = '';
+  for (let i = 0; i < s.length; i++) {
+    const element = s[i];
+    // if()
+  }
+
+  return 3;
+}
+
+console.log(test('merhaba'));
